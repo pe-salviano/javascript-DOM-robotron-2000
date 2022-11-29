6fef43a3931032f48d0f4aaaece6ff2bbb5e60a8
pe-salviano <pedro_s.c@hotmail.com>
2022-11-29 07:31:42 -0300

    curso em andamento

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
-const controle = document.querySelectorAll(".controle-ajuste")
+const controle = document.querySelectorAll("[data-controle]")
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (e) => {
-        manipulaDados(e.target.textContent, e.target.parentNode)
-        
+        manipulaDados(e.target.dataset.controle, e.target.parentNode)
     })
 })
     
 function manipulaDados(operacao, controle){
-    const peca = controle.querySelector(".controle-contador")
+    const peca = controle.querySelector("[data-contador]")
     
-    if (operacao ==="subtrair"){
+    if (operacao === "-"){
         peca.value = parseInt(peca.value) -1
     }else{
         peca.value = parseInt(peca.value) +1
     }
-
 }
-
-var lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"]; 
